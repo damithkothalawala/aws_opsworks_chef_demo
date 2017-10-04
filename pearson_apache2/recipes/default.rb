a20fa4539c00ae99557fe085d3a4de0f617ff085
damithkothalawala <admin@drklk.org>
2017-10-04 06:38:40 +0000

    test

--- pearson_apache2/recipes/default.rb
+++ pearson_apache2/recipes/default.rb
@@ -5,3 +5,8 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 include_recipe "apache2"
+
+file '/var/www/html/index.html' do
+  content "<h1>#{node['pearson_apache']['welcome_msg']}"
+  owner "apache"
+end
